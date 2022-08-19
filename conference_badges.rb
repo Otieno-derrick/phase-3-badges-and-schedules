2194f3878d63f81fb69e74c9f734a0fe215f7310
otieno-derrick <otieno.derrrick@student.moringaschool.com>
2022-08-19 12:07:38 +0300

    done

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,30 @@
 # Write your code here.
+def badge_maker (name)
+ "Hello, my name is #{name}."
+end
+
+
+
+def batch_badge_creator present
+  present.map do |attendee|
+    "Hello, my name is #{attendee}."
+  end
+end
+
+
+
+def assign_rooms present
+  present.map.with_index(1) do |attendee, room_no|
+    "Hello, #{attendee}! You'll be assigned to room #{room_no}!"
+  end
+end
+
+
+def printer present
+  batch_badge_creator(present).each do |badge|
+    puts badge
+  end
+   assign_rooms(present).each do |room_assigned|
+    puts room_assigned
+    end
+end
